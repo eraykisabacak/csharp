84235279fe4b674db3e590614b5827eff609642f
Eray Kısabacak <nefret19981998@hotmail.com>
2019-03-01 10:19:18 +0300

    First

---
+++ Diger_Forma_Bilgi_Aktarma/Diger_Forma_Bilgi_Aktarma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diger_Forma_Bilgi_Aktarma
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
