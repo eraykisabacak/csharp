5ac9b4d07cac63643cb0337c965c5454c43bbc6b
Eray Kısabacak <nefret19981998@hotmail.com>
2019-03-01 10:35:33 +0300

    Aşağı Yukarı Sağ Sola Giden Form

--- Sag_Sol_Yukari_Asagi/Sag_Sol_Yukari_Asagi/Form1.Designer.cs
+++ Sag_Sol_Yukari_Asagi/Sag_Sol_Yukari_Asagi/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.yukari = new System.Windows.Forms.Button();
+            this.asagi = new System.Windows.Forms.Button();
+            this.sol = new System.Windows.Forms.Button();
+            this.sag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // yukari
+            // 
+            this.yukari.Location = new System.Drawing.Point(357, 107);
+            this.yukari.Name = "yukari";
+            this.yukari.Size = new System.Drawing.Size(131, 41);
+            this.yukari.TabIndex = 0;
+            this.yukari.Text = "Yukari";
+            this.yukari.UseVisualStyleBackColor = true;
+            this.yukari.Click += new System.EventHandler(this.yukari_Click);
+            // 
+            // asagi
+            // 
+            this.asagi.Location = new System.Drawing.Point(357, 201);
+            this.asagi.Name = "asagi";
+            this.asagi.Size = new System.Drawing.Size(131, 41);
+            this.asagi.TabIndex = 1;
+            this.asagi.Text = "Aşağı";
+            this.asagi.UseVisualStyleBackColor = true;
+            this.asagi.Click += new System.EventHandler(this.asagi_Click);
+            // 
+            // sol
+            // 
+            this.sol.Location = new System.Drawing.Point(260, 154);
+            this.sol.Name = "sol";
+            this.sol.Size = new System.Drawing.Size(131, 41);
+            this.sol.TabIndex = 3;
+            this.sol.Text = "Sol";
+            this.sol.UseVisualStyleBackColor = true;
+            this.sol.Click += new System.EventHandler(this.sol_Click);
+            // 
+            // sag
+            // 
+            this.sag.Location = new System.Drawing.Point(453, 154);
+            this.sag.Name = "sag";
+            this.sag.Size = new System.Drawing.Size(131, 41);
+            this.sag.TabIndex = 2;
+            this.sag.Text = "Sağ";
+            this.sag.UseVisualStyleBackColor = true;
+            this.sag.Click += new System.EventHandler(this.sag_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sol);
+            this.Controls.Add(this.sag);
+            this.Controls.Add(this.asagi);
+            this.Controls.Add(this.yukari);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button yukari;
+        private System.Windows.Forms.Button asagi;
+        private System.Windows.Forms.Button sol;
+        private System.Windows.Forms.Button sag;
     }
 }
 
--- Sag_Sol_Yukari_Asagi/Sag_Sol_Yukari_Asagi/Form1.cs
+++ Sag_Sol_Yukari_Asagi/Sag_Sol_Yukari_Asagi/Form1.cs
@@ -12,9 +12,39 @@ namespace Sag_Sol_Yukari_Asagi
 {
     public partial class Form1 : Form
     {
+        Form2 fr2 = new Form2();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            fr2.Show();
+
+
+        }
+
+        private void yukari_Click(object sender, EventArgs e)
+        {
+            fr2.Top -= 25;
+        }
+
+        private void sol_Click(object sender, EventArgs e)
+        {
+            fr2.Left -= 25;
+        }
+
+        private void sag_Click(object sender, EventArgs e)
+        {
+            fr2.Left += 25;
+        }
+
+        private void asagi_Click(object sender, EventArgs e)
+        {
+            fr2.Top += 25;
         }
     }
 }
